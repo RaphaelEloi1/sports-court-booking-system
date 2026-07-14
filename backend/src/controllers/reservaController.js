1b8780bb9b2453641d3aa51cff3eea61e599f403
Jessica <jmaria.carvalho30@gmail.com>
2026-07-14 16:40:04 -0300

    feat: criar controller de reservas com regra de choque de horarios

---
+++ backend/src/controllers/reservaController.js
@@ -0,0 +1,57 @@
+const prisma = require("../database");
+
+async function criarReserva(request, response) {
+    try {
+        const { jogadorId, quadraId, data, inicio, fim } = request.body;
+
+        if (!jogadorId || !quadraId || !data || !inicio || !fim) {
+            return response.status(400).json({
+                mensagem: "Todos os campos (jogadorId, quadraId, data, inicio, fim) são obrigatórios.",
+            });
+        }
+
+
+        const dataHoraInicio = new Date(`${data}T${inicio}:00`);
+        const dataHoraFim = new Date(`${data}T${fim}:00`);
+
+        const reservaExistente = await prisma.reserva.findFirst({
+            where: {
+                quadraId: quadraId,
+                inicio: { lt: dataHoraFim },
+                fim: { gt: dataHoraInicio }
+            }
+        });
+
+
+        if (reservaExistente) {
+            return response.status(409).json({
+                mensagem: "Esta quadra já está reservada para este horário."
+            });
+        }
+
+        const novaReserva = await prisma.reserva.create({
+            data: {
+                jogadorId: jogadorId,
+                quadraId: quadraId,
+                inicio: dataHoraInicio,
+                fim: dataHoraFim
+            }
+        });
+
+        return response.status(201).json(novaReserva);
+
+
+
+    } catch (error) {
+        console.error(error);
+        return response.status(500).json({
+            mensagem: "Erro ao criar reserva.",
+        });
+    }
+
+}
+
+
+module.exports = {
+    criarReserva,
+};
